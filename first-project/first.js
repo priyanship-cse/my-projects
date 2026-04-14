5368753fcd684a3944b60c73ea3c198b3ab3bab2
riya9193 <priyanshipanwar348@gmail.com>
2026-04-14 23:29:23 +0530

    add background change button

--- first-project/first.js
+++ first-project/first.js
@@ -3,15 +3,29 @@
 let input = document.querySelector(".input");
 let button = document.querySelector(".button");
 let result = document.querySelector(".result");
+let bg = document.querySelector(".bg");
 
 button.addEventListener("click", function () {
     let value = input.value;
 
     if (input.value === "") {
         result.innerText = " Please Enter a Value";
-    } else if ( value % 2 == 0) {
-        result.innerText = " Even Number " ;
-    } else {
+        result.style.color = "black";
+    } else if ( value % 2 == 0 ) {
+        result.innerText = " Even Number  " ;
+         result.style.color = " red";
+    } else  {
         result.innerText = " Odd Number ";
+        result.style.color = "blue";
     }
+
+    
+});
+
+bg.addEventListener("click" , function(){
+        let r = Math.floor(Math.random()*256);
+         let g = Math.floor(Math.random()*256);
+          let b = Math.floor(Math.random()*256);
+           document.body.style.backgroundColor = `rgb(${r} , ${g},${b})`;
+
 });
